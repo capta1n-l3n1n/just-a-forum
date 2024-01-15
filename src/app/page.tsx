d8a1e3ce072837ac9993b47881717b655d46bbe5
Luong Sy Bao <baols@bluecore.vn>
2024-01-15 22:32:19 +0700

    fix: postgresql prisma schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,6 +17,7 @@ export default async function Home() {
       <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
         {/* @ts-expect-error server component */}
         {session ? <CustomFeed /> : <GeneralFeed />}
+        {/* <GeneralFeed /> */}
 
         {/* subreddit info */}
         <div className="overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
